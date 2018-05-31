489461fbd604284250af2c0f5ea6528fcdddae21
lijian <lijiancode@gmail.com>
2018-05-31 16:45:39 +0800

    v1.0

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,6 +3,7 @@
     <div class="btn_class">
       <el-button type="primary" @click="dialogFormVisible = true">添加任务</el-button>
       <el-button type="success" @click="allrun">全部开始</el-button>
+      <el-button type="info" @click="loaddata">获取结果</el-button>
     </div>
     <el-table :data="tableData" border align:center style="width: 100%;">
       <el-table-column type="index" header-align="center">
@@ -15,7 +16,7 @@
       </el-table-column>
       <el-table-column label="操作" header-align="center">
         <template slot-scope="scope">
-          <el-button size="mini" @click="handleStart(scope.$index, scope.row)">开始</el-button>
+          <el-button size="mini" @click="handleStart(scope.$index, scope.row)" v-bind:disabled="scope.row[2]==3 || scope.row[2]==2 || scope.row[2]==4">开始</el-button>
           <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
         </template>
       </el-table-column>
@@ -50,26 +51,33 @@ export default {
   },
   methods: {
     handleStart(index, row) {
-      Vue.set(this.tableData[index],2,2);
+      Vue.set(this.tableData[index], 2, 2);
       axios({
         url: "/download",
         method: "post",
         data: {
           id: this.tableData[index][0]
         }
-      }).then(res => {
-        this.loaddata();
-      },error=>{
-        this.loaddata();
-      });
+      }).then(
+        res => {
+          this.loaddata();
+        },
+        error => {
+          console.log("任务进行中...");
+        }
+      );
     },
     showstatus(row, column) {
       if (row[2] == 1) {
-        return "已完成";
+        return "下载失败";
       } else if (row[2] == 0) {
-        return "失败";
+        return "未开始";
       } else if (row[2] == 2) {
-        return "下载中..."
+        return "下载中...";
+      } else if (row[2] == 3) {
+        return "已完成";
+      } else if (row[2] == 4) {
+        return "禁止下载";
       } else {
         return "未开始";
       }
@@ -80,7 +88,7 @@ export default {
         method: "post",
         data: {
           id: this.tableData[index][0]
-        },
+        }
       }).then(res => {
         this.loaddata();
       });
@@ -102,16 +110,19 @@ export default {
       });
     },
     allrun() {
-      for(var i=0;i<this.tableData.length;i++){
-        if(this.tableData[i][2]==null){
-          Vue.set(this.tableData[i],2,2);
+      for (var i = 0; i < this.tableData.length; i++) {
+        if (this.tableData[i][2] == 0) {
+          Vue.set(this.tableData[i], 2, 2);
         }
       }
-      axios.get("/download").then(res => {
-        this.loaddata();
-      },error=>{
-        this.loaddata();
-      });
+      axios.get("/download").then(
+        res => {
+          this.loaddata();
+        },
+        error => {
+          console.log("任务进行中...");
+        }
+      );
     },
     loaddata() {
       axios.get("/getfile").then(res => {
